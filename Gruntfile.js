17d04e0108c4ee2aea68b8a81215b402bbf72dd6
Mitja Kramberger <mkramberger@atlassian.com>
2014-07-28 15:48:14 +1000

    added better way of loading grunt modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,11 @@ module.exports = function(grunt) {
 
   grunt.initConfig(config);
 
-  grunt.loadNpmTasks('grunt-node-webkit-builder');
-  grunt.loadNpmTasks('grunt-available-tasks');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-shell');
+  Object.keys(config.pkg.devDependencies).forEach(function(item) {
+    if (item.lastIndexOf('grunt-', 0) === 0) {
+      grunt.loadNpmTasks(item);
+    }
+  });
 
   grunt.registerTask('default', [ 'availabletasks' ]);
   grunt.registerTask('compile', [
@@ -38,7 +31,6 @@ module.exports = function(grunt) {
     'browserify:client',
     'less:dev'
   ]);
-
   grunt.registerTask('run-dev', [ 'connect:server:keepalive' ]);
   grunt.registerTask('run', [ 'shell:run' ]);
 };
